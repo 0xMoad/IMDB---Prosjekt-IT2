2611bfa44faa35190974b3ab60de077a3dc7c53c
TheDeezTroyer <151626833+TheDeezTroyer@users.noreply.github.com>
2023-12-02 13:51:12 +0100

    Helt ferdig film anbefaler
    Inneholder også kommentarer om hvordan koden funker

--- XfactorFeli.py
+++ XfactorFeli.py
@@ -10,37 +10,37 @@ import random as rd
 
 FILNAVN="imdb_top_1000.csv"
 
-L=[]
-G=[]
-IG=[]
+L=[]    #Liste med informasjon om alle filmene
+G=[]    #Liste over alle sjangerene. G for genre 
+IG=[]   #Todimensjonal liste som inneholder indexene til hver sjanger. IG -> index to genre
 
 with open(FILNAVN, encoding="utf-8-sig") as fil: 
     filinnhold = csv.reader(fil, delimiter=",")
     overskrifter = next(filinnhold)
     
-    for rad in filinnhold:
-        L.append(rad)
-        infoG=rad[5].split(",")
-        for i in range(len(infoG)):
-            if infoG[i].strip().lower() not in G:
-                G.append(infoG[i].strip().lower())
-        G = sorted(G)
+    for rad in filinnhold:  #Går gjennom alle radene i datastettet
+        L.append(rad)   #Legger til radene i listen L
+        infoG=rad[5].split(",")    #Finner sjangeren*e på filmen og legger de in en listen infoG (informasjon genre)
+        for i in range(len(infoG)):    #Siden det er flere sjangere per film, må jeg noen ganger gå gjennom flere
+            if infoG[i].strip().lower() not in G:    #Sjekker om sjangeren er i listen G
+                G.append(infoG[i].strip().lower())   #Hvis den ikke er det blir den lagt til i listen G
+        G = sorted(G)   #Sorterer listen G alfabetisk 
         
-    for i in range(len(G)):
-        index=[]
-        for j in range(len(L)):
-            infoG = L[j][5].split(",")
-            if G[i] in [info.strip().lower() for info in infoG]:
-                index.append(j)
-        IG.append(index)
+    for i in range(len(G)):    #Går gjennom alle sjangerene
+        index=[]    #lager en tom liste
+        for j in range(len(L)):    #Går gjennom listen L
+            infoG = L[j][5].split(",")    #Finner sjangeren*e til listen filmen L[j] 
+            if G[i] in [info.strip().lower() for info in infoG]:    #Og hvis sjangeren*e til filmen L[j] er den samme som G[i] 
+                index.append(j)    #Blir indexen til raden der man finner denne sjangeren lagt til i den tomme listen index
+        IG.append(index)    #Alle sjangerene får hver sine indexer i listen IG
 
 
-def filmForslag(sjanger):
-    if sjanger.lower() == "x":
-        filmIndex = rd.randint(0, len(L)-1)   
+def filmForslag(sjanger):    #Funksjon som skal foreslå en film basert på sjanger
+    if sjanger.lower().strip() == "x":
+        filmIndex = rd.randint(0, len(L)-1)   #Hvis man ikke har en preferanse til film får man en helt tilfeldig film
     else:
-        indexG = G.index(sjanger.lower())
-        filmIndex = IG[indexG][rd.randint(0, len(IG[indexG])-1)]
+        indexG = G.index(sjanger.lower())   #Finner indexen til sjangeren i listen G
+        filmIndex = IG[indexG][rd.randint(0, len(IG[indexG])-1)]    #Velger ut en tilfeldig film ut ifra indexene til sjangeren i datasettet, ved hjelp av den todemensjonale listen IG
         
     
     tittel=L[filmIndex][1]
@@ -76,10 +76,10 @@ satisfied = False
 while not satisfied:
     print("Ønsker du en annen anbefaling? ")
     satisfiedInput = input("\nSvar: ")
-    if satisfiedInput.lower() == "ja":
+    if satisfiedInput.lower().strip() == "ja":
         sjanger=input("Skriv sjanger her: ")
         filmForslag(sjanger)
-    elif satisfiedInput.lower() == "nei":
+    elif satisfiedInput.lower().strip() == "nei":
         satisfied = True
         print("\nHa en fin dag videre!")
     else:
